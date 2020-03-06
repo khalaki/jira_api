d1809a823f8df1f4fe553a32ced5a85b1344384b
Ivan <ivan.khalak@globallogic.com>
2020-03-06 14:27:37 +0200

    fix

---
+++ vars/jiraComment_test.groovy
@@ -0,0 +1,42 @@
+def call(body) {
+
+    //Set JIRA site and regex
+    env.JIRA_URL='https://xalak.atlassian.net'
+    env.JIRA_REG='ABJ-\\d*'
+
+    //Set fix build variable
+    if (currentBuild?.getPreviousBuild()?.result == 'FAILURE') {
+        if (currentBuild.result == 'SUCCESS') {
+            env.FIXED_BUILD = "1"
+        }
+    }
+    
+    //Set build result variable
+    env.BUILD_RESULT = currentBuild.result
+
+    //Set git commit variables
+    try {
+        env.GIT_PREVIOUS_COMMIT = currentBuild.previousBuild.buildVariables.GIT_COMMIT
+    } catch(Exception ex) {
+        println("No GIT_PREVIOUS_COMMIT variable")
+    }
+    try {
+        env.GIT_PREVIOUS_SUCCESSFUL_COMMIT = currentBuild.previousSuccessfulBuild.buildVariables.GIT_COMMIT
+    } catch(Exception ex) {
+        println("No GIT_PREVIOUS_SUCCESSFUL_COMMIT variable")
+    }
+
+    //Get JIRA api script
+    copyArtifacts filter: 'devops/jira_api.sh', fingerprintArtifacts: true, projectName: 'jira_api', selector: lastSuccessful()
+
+    //Set credential variable JIRA_CRED="super@user.xyz:token"
+    withCredentials([usernameColonPassword(credentialsId: '255bc25b-6216-420b-9880-96e9a1989913', variable: 'JIRA_CRED')]) {
+        sh """
+            cd devops
+            chmod +x jira_api.sh
+            ./jira_api.sh
+        """
+    archiveArtifacts 'devops/*'
+    }
+
+}
